57ebb70eaa4c37b5970636c1ccdbb0e15b431aa1
Denise87 <c095060@corp.caixa.gov.br>
2019-06-27 17:31:59 -0300

    atualizacao

--- js/index.js
+++ js/index.js
@@ -1,42 +1,62 @@
 
-    // $('.carousel').carousel();
-    // setInterval(function() {
-    //   $('.carousel').carousel('next');
-    // }, 10000);
+ 	$(function() {
+        var body = $('body');
+        var backgrounds = new Array(
+        'url(img/imagem1.jpg)',
+        'url(img/imagem2.jpg)',
+        'url(img/imagem3.jpg)'
+        );
+        var current = 0;
+        
+        function nextBackground() {
+        body.css(
+        'background',
+        backgrounds[current = ++current % backgrounds.length],
+       
+        );
+     
+        setTimeout(nextBackground, 10000);
+        body.css('background-repeat', "no-repeat");
+        body.css('background-size','cover');
+        }
+        setTimeout(nextBackground, 10000);
+        body.css('background', backgrounds[0]);
+        body.css('background-repeat', "no-repeat");
+        body.css('background-size','cover');
+        });
+
     $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         height:520,
         indicators: true,
-    
-       
-      });
-      setInterval(function() {
-        $('.carousel').carousel('next');
-      }, 10000);
+    });
+    //   setInterval(function() {
+    //     $('.carousel').carousel('next');
+    //   }, 10000);
 
       var ctx = document.getElementById('myChart').getContext('2d');
       var myChart = new Chart(ctx, {
-          type: 'bar',
+       
+        type: 'bar',
           data: {
-              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+              
+              labels: ['Comex', 'Bndes', 'SIGCB', 'Middle'],
               datasets: [{
                   label: 'Indicadores',
                   data: [12, 19, 3, 5, 2, 3],
                   backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      
                   ],
                   borderColor: [
                       'rgba(255, 99, 132, 1)',
                       'rgba(54, 162, 235, 1)',
                       'rgba(255, 206, 86, 1)',
                       'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
+                      
                   ],
                   borderWidth: 1
               }]
@@ -51,3 +71,34 @@
               }
           }
       });
+      var chart2 = document.getElementById('chartLinhaColuna')
+      var chartLinhaColuna = new Chart(chart2, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: 'Relizado em dias',
+                data: [1,1,1,5],
+                backgroundColor: 
+                    'rgba(255, 99, 132, 0.5)'
+             
+            }, {
+                label: 'Objetivo',
+                data: [3,3,3,3],
+                backgroundColor: 
+                'rgba(211, 211, 211, 1)',
+                // Changes this dataset to become a line
+                type: 'line'
+            }],
+            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril']
+        },
+        options: {
+            scales: {
+                
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
